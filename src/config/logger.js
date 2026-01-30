e7179cb94dfda3baadfceb1143d0a7c645a86fa0
Vishal <vishuraina0001@gmail.com>
2026-01-30 16:31:18 +0530

    Added Winston logger with Singleton pattern

---
+++ src/config/logger.js
@@ -0,0 +1,33 @@
+const winston = require('winston');
+
+class Logegr{
+    constructor(){
+        if(Logger.instance){
+            return Logger.instance;
+        }
+
+        this.logger = winston.createLogger({
+            level: 'info',
+            format: winston.format.combine(
+                winston.format.timestamp(),
+                winston.format.json()
+            ),
+            transports: [
+                new winston.transports.Console({
+                    format: winston.format.simple(),
+                }),
+            ],
+        });
+
+        Logger.instance = this;
+    }
+
+    static getInstance(){
+        if(!Logger.instance){
+            Logger.instance = new Logger();
+        }
+        return Logger.instance.logger;
+    }
+}
+
+module.exports = Logger.getInstance();
